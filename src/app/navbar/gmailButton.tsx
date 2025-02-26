1b9f12de955810f5b2a1e9bc8ba3ab7a2b97f5b7
Ben Doepker <bendoepk@gmail.com>
2025-02-26 07:32:18 -0500

    updated css for mobile devices

--- src/app/navbar/gmailButton.tsx
+++ src/app/navbar/gmailButton.tsx
@@ -5,13 +5,15 @@ import "./gmailButton.css"
 export default function GmailButton() {
     const [effect, setEffect] = useState("gmail");
     return (
-        <button className={effect}
-            onClick={() => {
-                navigator.clipboard.writeText("bendoepk@gmail.com");
-                setEffect("gmail_animated");}}
-            onAnimationEnd={() => {setEffect("gmail")}}>
-            <img alt=""/>
-            <div className="copied_text">Copied!</div>
-        </button>
+        <div className="gmail_container">
+            <button className={effect}
+                onClick={() => {
+                    navigator.clipboard.writeText("bendoepk@gmail.com");
+                    setEffect("gmail_animated");}}
+                onAnimationEnd={() => {setEffect("gmail")}}>
+                <img alt=""/>
+                <div className="copied_text">Copied!</div>
+            </button>
+        </div>
     );
 }
